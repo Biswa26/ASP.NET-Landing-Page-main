db32961ff03658301689f46501a6304b88b2294e
Biswajit Behera <biswajitbehera910@gmail.com>
2026-01-01 13:53:44 +0530

    changes

--- obj/Release/net8.0/Indotalent.AssemblyInfo.cs
+++ obj/Release/net8.0/Indotalent.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Indotalent")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e2d2a74515944a52b6cef98b69a713e2cb2a106")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e553bb9655ebd4751bd1b8483ed52a7cd49ad66e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Indotalent")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Indotalent")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
